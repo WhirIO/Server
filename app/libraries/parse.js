eff385a48f282666777f3e73adec70c65c132fd1
Stefan Aichholzer <theaichholzer@gmail.com>
2016-09-06 16:12:43 +0800

    Using roli.

--- app/libraries/parse.js
+++ app/libraries/parse.js
@@ -1,20 +1,17 @@
 'use strict';
 
 
-let uuid = require('uuid');
+let uuid = require('uuid'),
+    roli = require('roli');
 
 module.exports = {
 
     headers: (socket, req, next) => {
 
-        /**
-         * TODO
-         * Use roli.whir.io for a nice channel- and username.
-         */
         socket.whir = {
             headers: {
-                channel: socket.upgradeReq.headers['x-whir-channel'] || uuid.v4(),
-                username: socket.upgradeReq.headers['x-whir-username'] || uuid.v4(),
+                channel: socket.upgradeReq.headers['x-whir-channel'] || roli({ separator: '' }),
+                username: socket.upgradeReq.headers['x-whir-username'] || roli({ separator: '' }),
                 maxUsers: socket.upgradeReq.headers['x-whir-max-users'] || 1000,
                 timeout: socket.upgradeReq.headers['x-whir-timeout'] || 0,
                 sessionId: socket.upgradeReq.headers['x-whir-session-id'] || null
